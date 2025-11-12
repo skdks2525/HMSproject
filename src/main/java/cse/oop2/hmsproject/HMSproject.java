fcc2b693460c46d95aa0d00114af2b0e7479cf47
user <user@DESKTOP-S75NIE0>
2025-11-12 20:57:26 +0900

    테스트

---
+++ src/main/java/cse/oop2/hmsproject/HMSproject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cse.oop2.hmsproject;
+
+/**
+ *
+ * @author user
+ */
+public class HMSproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
